faf5a2e705bbc47e18228d244db37107b51b50a6
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-04-07 14:39:12 -0700

    configure jwt token

--- finance_backend/finance_backend/settings.py
+++ finance_backend/finance_backend/settings.py
@@ -10,6 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
