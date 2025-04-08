4105de61102d15289661342a3fef219533eb4bc5
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-04-07 19:50:18 -0700

    feat: add register endpoint to URLs
    - Added URL pattern for user registration at /register/
    - Connected to RegisterView for handling user sign-up

--- finance_backend/core/urls.py
+++ finance_backend/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import (
+    RegisterView,
     CustomUserViewSet,
     TransactionViewSet,
     TransactionCategoryViewSet,
@@ -16,6 +17,7 @@ router.register(r"budget", BudgetViewSet, basename="Budget")
 
 
 urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
