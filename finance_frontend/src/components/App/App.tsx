b3b248ec13b4fde8ad15b7f9ee29d819b39b93b2
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-05-06 19:24:12 -0700

    configure project for shadcn components

--- finance_frontend/src/components/App/App.tsx
+++ finance_frontend/src/components/App/App.tsx
@@ -1,11 +1,12 @@
+import { Button } from '../ui/button'
 import './App.css'
 
 const App = () => {
 
 
   return (
-    <div className='text-9xl text-red-500 bg-black p-50 justify-center flex items-center'>
-      Talwind is working
+    <div className='text-9xl text-blue-500 bg-black p-50 justify-center flex items-center'>
+      <Button variant={'destructive'} >Delete</Button>
     </div>
   )
 }
