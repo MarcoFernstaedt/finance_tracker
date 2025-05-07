31addd68c4064926ae30bad44dc3c17057fae01e
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-05-06 18:45:52 -0700

    frontend project setup taildwindcss react 19

---
+++ finance_frontend/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+
+
+  return (
+    <div className='text-9xl text-red-500 bg-black p-50 justify-center flex items-center'>
+      Talwind is working
+    </div>
+  )
+}
+
+export default App
