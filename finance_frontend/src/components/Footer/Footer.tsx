ae06543a7892c8dfabc6e93db1a970d25538dae2
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-05-12 18:31:21 -0700

    style: background color update gray

--- finance_frontend/src/components/Footer/Footer.tsx
+++ finance_frontend/src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='w-full bg-blue-50 py-12'>
-            <div className="container mx-auto px-4 text-center text-gray-600">
+        <footer className='w-full bg-gray-900 py-12'>
+            <div className="container mx-auto px-4 text-center text-gray-500">
                 Made by Marco Fernstaedt
             </div>
         </footer>
