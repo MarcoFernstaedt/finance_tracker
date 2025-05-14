6a9e6cd5b00c79d2822afe74b87ba66c957e034c
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-05-13 20:52:48 -0700

    implament url patterns

--- finance_backend/accounts/urls.py
+++ finance_backend/accounts/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenVerifyView
+from .views import RegisterView, LoginView, LogoutView, RefreshTokenView
 
 urlpatterns = [
-    # path("auth/signin", include("accounts.urls")),
+    path("auth/signin", LoginView.as_view(), name="login"),
+    path("auth/register", RegisterView.as_view(), name="register"),
+    path("auth/signout", LogoutView.as_view(), name="logout"),
+    path("auth/refresh", RefreshTokenView.as_view(), name="refresh_token"),
+    path("auth/verify", TokenVerifyView.as_view(), name="token_verify"),
 ]
