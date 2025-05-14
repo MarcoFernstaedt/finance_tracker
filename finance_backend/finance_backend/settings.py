2d3bbd11c3389d938f60045901721d3b608ce04a
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-05-13 19:49:20 -0700

    configure to allow CORS

--- finance_backend/finance_backend/settings.py
+++ finance_backend/finance_backend/settings.py
@@ -50,11 +50,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "accounts",
+    "corsheaders",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -63,6 +65,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "finance_backend.urls"
 
