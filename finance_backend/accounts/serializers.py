2ed0814d7e1d4d289834627d34b2f920bc50e501
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-05-13 19:08:37 -0700

    fix inherit

--- finance_backend/accounts/serializers.py
+++ finance_backend/accounts/serializers.py
@@ -1,13 +1,14 @@
-from rest_framework.simplejwt.serializers impoert TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class CustomTokenObtainPairSerializer(CustomTokenObtainPairSerializer):
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        
-        data['user'] = {
-            'id': self.user.id,
-            'name': self.user.name,
-            'email': self.user.email
+
+        data["user"] = {
+            "id": self.user.id,
+            "name": self.user.name,
+            "email": self.user.email,
         }
-        
+
         return data
